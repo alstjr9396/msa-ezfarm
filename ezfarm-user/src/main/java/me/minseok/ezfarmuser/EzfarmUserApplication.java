c945437c85fd9e0fdb3a23119e349c5f072c5c2c
kang <minseokkang95@gmail.com>
2022-04-11 16:24:35 +0900

    init commit(ezfarm-user)

---
+++ ezfarm-user/src/main/java/me/minseok/ezfarmuser/EzfarmUserApplication.java
@@ -0,0 +1,13 @@
+package me.minseok.ezfarmuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzfarmUserApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EzfarmUserApplication.class, args);
+  }
+
+}
---
+++ ezfarm-user/src/test/java/me/minseok/ezfarmuser/EzfarmUserApplicationTests.java
@@ -0,0 +1,13 @@
+package me.minseok.ezfarmuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzfarmUserApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

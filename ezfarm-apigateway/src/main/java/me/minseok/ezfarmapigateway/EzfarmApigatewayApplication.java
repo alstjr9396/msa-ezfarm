e797e201dde9ddd07596c0c2e92496f10dfa79a0
kang <minseokkang95@gmail.com>
2022-04-12 21:05:59 +0900

    init commit(ezfarm-apigateway)

---
+++ ezfarm-apigateway/src/main/java/me/minseok/ezfarmapigateway/EzfarmApigatewayApplication.java
@@ -0,0 +1,13 @@
+package me.minseok.ezfarmapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzfarmApigatewayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EzfarmApigatewayApplication.class, args);
+  }
+
+}
---
+++ ezfarm-apigateway/src/test/java/me/minseok/ezfarmapigateway/EzfarmApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package me.minseok.ezfarmapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzfarmApigatewayApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
